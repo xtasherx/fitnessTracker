b882df90ce4761d85a8de793d5cf656162c7c4ca
Tasha <xtasherx@gmail.com>
2020-11-14 21:30:37 -0600

    updated env variable for mongodb/heroku deployment

--- server.js
+++ server.js
@@ -14,7 +14,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workouts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 // html routes
 app.get("/exercise", (_req, res) => {
